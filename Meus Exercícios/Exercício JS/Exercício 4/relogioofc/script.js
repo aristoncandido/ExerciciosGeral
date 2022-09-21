ade71f20c3e3fa9082a0b7009d3776a1c0ad4e2f
Ariston <aristoncandido.nw@gmail.com>
2022-09-21 12:27:11 -0300

    coloquei a data no projeto do relogio digital

--- Meus Exercícios/Exercício JS/Exercício 4/relogioofc/script.js
+++ Meus Exercícios/Exercício JS/Exercício 4/relogioofc/script.js
@@ -2,7 +2,7 @@ function ShowTime(){
 
     var rel = document.getElementById("horario");
     var m = document.getElementById("turno");
-
+    var d = document.getElementById("data")
 
 
     var time = new Date();
@@ -13,27 +13,82 @@ function ShowTime(){
 
     var segundo = time.getSeconds();
 
+    var data  = time.getDay();
+
     var turno ;
 
-    if(hora <12){
+    if(hora < 12){
          turno = "AM"
-    }else if(hora>12){
+    }else if(hora >= 12){
         turno = "PM"
     }
+
+
+
+    switch(data){
+       
+        case 0:
+            data = "Domingo";
+
+        break;
+
+        case 1 :
+            
+        data = "Segunda-Feira";
+
+        break;
+
+        case 2 :
+
+        data = "Terça-Feira";
+        break;
+
+        case 3 :
+
+        data = "Quarta-Feira";
+        break;
+
+        case 4 :
+            data = "Quinta-Feira";
+        break;
+         
+        case 5 :
+            data = "Sexta-Feira";
+
+         break;
+         
+         
+         case 6 :
+            data = "Sábado";
+
+          break;  
+    }
+
+
+
+
+
+
+
 
     /*formatação do SEGUNDOS, HORA E MINUTOS */
     if(segundo < 10 ){
 
         rel.innerHTML = `${hora}:${minuto}:0${segundo}`;
         m.innerHTML = `${turno}`;
+        d.innerHTML = `${data}`;
 
     }else{
               
       rel.innerHTML = `${hora}:${minuto}:${segundo}`;
       m.innerHTML = `${turno}`;
+      d.innerHTML = `${data}`;
 
     }
    
+
+
+
 
 
 
